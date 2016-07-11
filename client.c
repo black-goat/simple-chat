bc1d13b82de2f08f7fb2578d1b7073d79e449269
Bozhidar Vasilev <blackgoathooves@gmail.com>
2016-07-11 23:47:37 +0300

    Chat working!
    Chat between two local machines now working. Still needs better
    interface.

--- client.c
+++ client.c
@@ -1,30 +1,75 @@
 #include <windows.h>
-#include <winsock.h>
+#include <winsock2.h>
 #include <stdio.h>
 
 #define NETWORK_ERROR -1
 #define NETWORK_OK     0
 
+#define HOST_BY_NAME   0
+#define HOST_BY_ADDR   1
+
+#define MODE_SEND   0
+#define MODE_RECV   1
+
 void ReportError( int errorCode, const char* whichFunc );
 
 char* readLine( SOCKET s );
+int is_exit_string( char* str );
+
+void init_winsock( WORD version );
+int get_host_info( LPHOSTENT hostInfo, int getHostBy, const char* host )
+{
+    if( getHostBy == HOST_BY_NAME )
+    {
+        hostInfo = gethostbyname( host );
+    }
+    else if( getHostBy == HOST_BY_ADDR )
+    {
+        IN_ADDR iaHost;
+        iaHost.s_addr = inet_addr( host );
+        hostInfo = gethostbyaddr( (const char*)iaHost.s_addr, sizeof( struct in_addr ), AF_INET );
+    }
+    else
+    {
+        MessageBox( NULL, "Error: No such command %d", "get_host_info() failed", 0 );
+    }
+
+    printf( "%d\n", (hostInfo == NULL ) );
+
+    if( !hostInfo )
+    {
+        int nret = WSAGetLastError();
+        switch( getHostBy )
+        {
+        case HOST_BY_NAME:
+            ReportError( nret, "gethostbyname()" );
+            break;
+        case HOST_BY_ADDR:
+            ReportError( nret, "gethostbyaddr()" );
+            break;
+        default:
+            MessageBox( NULL, "Invalid operation requested", "get_host_by() failed", 0 );
+            break;
+        }
+
+        return -1;
+    }
+
+    return 0;
+}
 
 int WINAPI WinMain( HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmd, int nShow )
 {
-    WORD sockVersion;
-    WSADATA wsaData;
-    int nret;
+    int nret; // Used for error reporting
 
-    sockVersion = MAKEWORD(1, 1);
-
-    WSAStartup( sockVersion, &wsaData );
+    init_winsock( MAKEWORD( 2, 0 ) );
 
     LPHOSTENT hostInfo;
     IN_ADDR iaHost;
 
-    iaHost.s_addr = inet_addr("46.228.47.114");
+    iaHost.s_addr = inet_addr("192.168.100.5");
 
-    hostInfo = gethostbyname("yahoo.com");
+    hostInfo = gethostbyname("192.168.100.5");
     //hostInfo = gethostbyaddr( (const char*)&iaHost.s_addr, sizeof( struct in_addr ), AF_INET );
 
     if (!hostInfo)
@@ -72,8 +117,57 @@ int WINAPI WinMain( HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmd, int nShow
 		return NETWORK_ERROR;
 	}
 
+    char* message = readLine( theSocket );
+    printf( "%s\n", message );
+
+    int mode = MODE_SEND;
+
+    while( 1 )
+    {
+        if( mode == MODE_SEND )
+        {
+            printf( "You: " );
+            int pos = 0;
+            char buffer[ 256 ];
+            ZeroMemory( buffer, 256 );
+            char c;
+            do{
+                c = getchar();
+                buffer[ pos ] = c;
+                ++pos;
+            }while( c != '\n' );
+            send( theSocket, buffer, strlen( buffer ), 0 );
+            if( is_exit_string( buffer ) )
+            {
+                mode = MODE_RECV;
+            }
+        }
+        else
+        {
+            char* theMessage;
+            theMessage = readLine( theSocket );
+            if( is_exit_string( theMessage ) )
+            {
+                mode = MODE_SEND;
+            }
+
+            printf( "Other: %s\n", theMessage );
+        }
+    }
+
+    system( "PAUSE" );
+
 	closesocket( theSocket );
+	WSACleanup();
 	return NETWORK_OK;
+}
+
+int is_exit_string( char* str )
+{
+    if( str[0] == '$' && (str[1] == '\n' || str[1] == NULL ) )
+        return 1;
+    else
+        return 0;
 }
 
 char* readLine( SOCKET s )
@@ -83,10 +177,10 @@ char* readLine( SOCKET s )
     char buffer;
     int bytesReceived;
 
+    int pos = 0;
+
     while( TRUE )
     {
-        int pos = 0;
-
         bytesReceived = recv(   s,
                                 &buffer,
                                 1,
@@ -119,4 +213,13 @@ void ReportError( int errorCode, const char* whichFunc )
     sprintf( errorMsg, "Call to %s returned error %d", (char *)whichFunc, errorCode );
 
     MessageBox( NULL, errorMsg, NULL, MB_OK );
+}
+
+void init_winsock( WORD version )
+{
+    WSADATA wsaData;
+
+    WSAStartup( version, &wsaData );
+
+    return;
 }
--- server.c
+++ server.c
@@ -5,7 +5,13 @@
 #define NETWORK_ERROR -1
 #define NETWORK_OK     0
 
+#define MODE_SEND   0
+#define MODE_RECV   1
+
 void ReportError( int errorCode, const char* whichFunc );
+
+char* readLine( SOCKET s );
+int is_exit_string( char* str );
 
 int WINAPI WinMain( HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmd, int nShow )
 {
@@ -54,8 +60,6 @@ int WINAPI WinMain( HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmd, int nShow
 
     nret = listen( listeningSocket, 1 );
 
-
-
     if( nret == SOCKET_ERROR )
     {
         nret = WSAGetLastError();
@@ -67,6 +71,7 @@ int WINAPI WinMain( HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmd, int nShow
 
     SOCKET theClient;
 
+    printf( "Awaiting connections...\n" );
     theClient = accept(     listeningSocket,
                             NULL,
                             NULL );
@@ -78,14 +83,95 @@ int WINAPI WinMain( HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmd, int nShow
         WSACleanup();
         return NETWORK_ERROR;
     }
-
     printf( "Client has connected!\n" );
+
+    char welcomeMessage[] = "Welcome to the server!\n";
+    send( theClient, welcomeMessage, strlen( welcomeMessage ), 0 );
+
+    int mode = MODE_RECV;
+
+    while( 1 )
+    {
+        if( mode == MODE_SEND )
+        {
+            printf( "You: " );
+            int pos = 0;
+            char buffer[ 256 ];
+            ZeroMemory( buffer, 256 );
+            char c;
+            do{
+                c = getchar();
+                buffer[ pos ] = c;
+                ++pos;
+            }while( c != '\n' );
+            send( theClient, buffer, strlen( buffer ), 0 );
+            if( is_exit_string( buffer ) )
+            {
+                mode = MODE_RECV;
+            }
+        }
+        else
+        {
+            char* theMessage;
+            theMessage = readLine( theClient );
+            if( is_exit_string( theMessage ) )
+            {
+                mode = MODE_SEND;
+            }
+
+            printf( "Other: %s\n", theMessage );
+        }
+    }
 
     closesocket( theClient );
     closesocket( listeningSocket );
 
+    system( "PAUSE" );
+
     WSACleanup();
     return NETWORK_OK;
+}
+
+char* readLine( SOCKET s )
+{
+    char* theMessage = (char*) malloc( 256 );
+    ZeroMemory( theMessage, 256 );
+    char buffer;
+    int bytesReceived;
+
+    int pos = 0;
+
+    while( TRUE )
+    {
+        bytesReceived = recv(   s,
+                                &buffer,
+                                1,
+                                0 );
+
+        if( bytesReceived <= 0 )
+        {
+            MessageBox( NULL, "recv() returned nothing.", NULL, MB_OK );
+            return NULL;
+        }
+
+        if( buffer != '\0' && buffer != '\n' )
+        {
+            theMessage[ pos ] = buffer;
+            ++pos;
+        }
+        else
+        {
+            return theMessage;
+        }
+    }
+}
+
+int is_exit_string( char* str )
+{
+    if( str[0] == '$' && (str[1] == '\n' || str[1] == NULL ) )
+        return 1;
+    else
+        return 0;
 }
 
 void ReportError( int errorCode, const char* whichFunc )
